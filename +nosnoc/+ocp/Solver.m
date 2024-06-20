b8c8fc95498bd3b7daff31b4d393fb7c9ab471fa
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-06-20 15:58:28 +0200

    add disc OCP

--- +nosnoc/+ocp/Solver.m
+++ +nosnoc/+ocp/Solver.m
@@ -44,7 +44,11 @@ classdef Solver < handle
                 obj.discrete_time_problem = nosnoc.discrete_time_problem.Heaviside(obj.dcs, opts);
                 obj.discrete_time_problem.populate_problem();
               case "nosnoc.model.Cls"
-                error("not implemented")
+                obj.dcs = nosnoc.dcs.Cls(model);
+                obj.dcs.generate_variables(opts);
+                obj.dcs.generate_equations(opts);
+                obj.discrete_time_problem = nosnoc.discrete_time_problem.Cls(obj.dcs, opts);
+                obj.discrete_time_problem.populate_problem();
               case "nosnoc.model.Pds"
                 obj.dcs = nosnoc.dcs.Gcs(model);
                 obj.dcs.generate_variables(opts);
